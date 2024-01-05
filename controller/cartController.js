0e6e852641520a3a9b79399a5d47014799b5ea78
AbdirahmanMoahemd <kaah6978@gmail.com>
2024-01-05 09:13:46 +0300

    test

--- controller/cartController.js
+++ controller/cartController.js
@@ -85,3 +85,84 @@ export const addToWishlist  = async(req, res) => {
     }
 
 }
+
+
+
+export const removeCartItem  = async(req, res) => {
+    try{
+
+        const {userId, productId} = req.body;
+
+        const product = await Product.findById(productId);
+
+
+        let user = await Users.findById(userId).populate("cart.product").populate("wishlist.product")
+       
+
+        for (let index = 0; index < user.cart.length; index++) {
+            if (user.cart[index].product._id.equals(product._id)) {
+                if (user.cart[i].quantity == 1) {
+                    user.cart.splice(index, 1)
+                    
+                }else{
+                    user.cart[index].quantity-=1 ;
+                }
+                
+            }
+            
+        }
+
+
+        user =await user.save()
+        res.status(200).json(user)
+
+    }catch(e){
+        res.status(500).json({ error: e.message });
+    }
+
+}
+
+
+
+
+export const deleteCartItem  = async(req, res) => {
+    try{
+
+        const {userId, index} = req.body;
+
+        let user = await Users.findById(userId).populate("cart.product").populate("wishlist.product")
+       
+        if (user) {
+            user.cart.splice(index, 1)            
+        }
+
+        user =await user.save()
+        res.status(200).json(user)
+
+    }catch(e){
+        res.status(500).json({ error: e.message });
+    }
+
+}
+
+
+export const deleteWishlistItem  = async(req, res) => {
+    try{
+
+        const {userId, index} = req.body;
+
+        let user = await Users.findById(userId).populate("cart.product").populate("wishlist.product")
+       
+        if (user) {
+            user.wishlist.splice(index, 1)            
+        }
+
+        user =await user.save()
+        res.status(200).json(user)
+
+    }catch(e){
+        res.status(500).json({ error: e.message });
+    }
+
+}
+
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { addToCart, addToWishlist } from '../controller/cartController.js';
+import { addToCart, addToWishlist, deleteCartItem, deleteWishlistItem, removeCartItem } from '../controller/cartController.js';
 
 const router = express.Router()
 
 
 router.route('/').post(addToCart)
 router.route('/add-to-wishlist').post(addToWishlist)
+router.route('/remove-cart-item').post(removeCartItem)
+router.route('/delete-cart-item').post(deleteCartItem)
+router.route('/delete-wishlist-item').post(deleteWishlistItem)
 
 
 export default router
